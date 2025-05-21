8a490a9c57a19bb8295854eef046fee6842136c1
Emmanuel Bedzo <bedzoemmanuel8@gmail.com>
2025-05-21 06:36:11 -0500

    Understanding how the App.jsx file works

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello React</h1>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, modi. Nemo dicta, hic laboriosam obcaecati facilis dignissimos minus eaque pariatur in ut doloribus ea temporibus molestias sit similique incidunt voluptatem.</h2>
     </>
   )
 }
