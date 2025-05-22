fa98967da443c9250de220f7470a125f3f99e5e4
Emmanuel Bedzo <bedzoemmanuel8@gmail.com>
2025-05-21 19:58:56 -0500

    Export the Todo Componet, and display it in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './components/Todo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,22 +10,9 @@ function App() {
   return (
     <>
     <h1>My Todos</h1>
-    <div>
-      <h2>Finish React Course</h2>
-      <button>Delete</button>
-    </div>
-    <div>
-      <h2>Finish ALX Course</h2>
-      <button>Delete</button>
-    </div>
-    <div>
-      <h2>Earn From Freelancing </h2>
-      <button>Delete</button>
-    </div>
-    <div>
-      <h2>Enjoy Financial Freedom React Course</h2>
-      <button>Delete</button>
-    </div>
+    <Todo />
+    <Todo />
+    <Todo />
     </>
   )
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,8 @@
+function Todo(){
+    return(
+        <div>
+            <h2>Finish React Crash Course</h2>
+        </div>
+    )
+}
+export default Todo;
