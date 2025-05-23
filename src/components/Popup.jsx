ddb592a380d68da50241b83493c93a71f33e8c0d
Emmanuel Bedzo <bedzoemmanuel8@gmail.com>
2025-05-23 04:19:16 -0500

    Make the popup component dynamic with Props

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,8 +1,8 @@
-function Popup (){
+function Popup ({title}){
     return (
         <>
             <div className="popup">
-                <span>Are you sure?</span>
+                <span>{title}</span>
                 <div className="popup__btns">
                     <button className="popup__btn">Confirm</button>
                     <button className="popup__btn popup__btn--cancel">Cancel</button>
