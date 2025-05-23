3e4c19b4e7260f0595ef8f61fc21718cb5e20ca6
Emmanuel Bedzo <bedzoemmanuel8@gmail.com>
2025-05-23 04:25:08 -0500

    Handling events using separate functions

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,15 @@
 function Todo({task, description}){
-    console.log(task);
+    function handleClick(){
+        console.log("Button Clicked")
+        
+    }
     
     return(
         <div className="todo-item">
             <h2>{task}</h2>
             <p>{description}</p>
             <button onClick={() =>{
-                console.log("Button Clicked");
-                
+                handleClick()
             }}>Delete</button>
         </div>
     )
