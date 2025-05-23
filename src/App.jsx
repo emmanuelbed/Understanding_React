dc9a7cad38d70a28595956dccf8fa16fa2dd5241
Emmanuel Bedzo <bedzoemmanuel8@gmail.com>
2025-05-23 04:54:11 -0500

    Conditional rendering basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Todo from './components/Todo'
-import TestComponent from './components/TestComponent'
-import Popup from './components/Popup'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Todo from "./components/Todo";
+import TestComponent from "./components/TestComponent";
+import Popup from "./components/Popup";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let popupOpen = false;
 
   return (
     <>
-    
-    <TestComponent />
-    <div>
-      <input type="text" onChange={(event) =>{
-        console.log(event.target.value);
-        
-      }}/>
-      <button>Add to do</button>
-    </div>
+      <TestComponent />
+      <div>
+        <input
+          type="text"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
+        <button>Add to do</button>
+      </div>
 
-    <Todo 
-    task = "Learn React" 
-    />
+      <Todo task="Learn React" />
 
-    <Todo 
-    task = "Finish ASAP"
-    />
+      <Todo task="Finish ASAP" />
 
-    <Todo 
-    task = "Land a job"
-    />
-    <Todo 
-    task = "Earn 100k"
-    />
-    <Todo 
-    task = "Land a job"
-    />
-    <Popup  title= "Are you Okay?" /> 
+      <Todo task="Land a job" />
+      <Todo task="Earn 100k" />
+      <Todo task="Land a job" />
+      {popupOpen ? <Popup title="Are you Okay?" /> : null}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,19 @@
-function Todo({task}){
-    function deleteHandler(){
-        console.log("Delete", task)
-        
-    }
-    
-    return(
-        <div className="todo-item">
-            <h2>{task}</h2>
-            <button onClick={() =>{
-                deleteHandler()
-            }}>Delete</button>
-        </div>
-    )
+function Todo({ task }) {
+  function deleteHandler() {
+    console.log("Delete", task);
+  }
+
+  return (
+    <div className="todo-item">
+      <h2>{task}</h2>
+      <button
+        onClick={() => {
+          deleteHandler();
+        }}
+      >
+        Delete
+      </button>
+    </div>
+  );
 }
 export default Todo;
