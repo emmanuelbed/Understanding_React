091acca886802fa9012a89b7b0cc93667d99318b
Emmanuel Bedzo <bedzoemmanuel8@gmail.com>
2025-05-24 19:11:51 -0500

    Add a new Posts Component and setting it up for routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Posts from "./pages/Posts";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/posts" element={<Posts/>}></Route>
       </Routes>
       <div></div>
     </Router>
