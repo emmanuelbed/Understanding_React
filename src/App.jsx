b152de3eb17ba038ec6fc4076be03048f480139c
Emmanuel Bedzo <bedzoemmanuel8@gmail.com>
2025-05-24 19:18:49 -0500

    Undrstanding the power of Link Component from the react Router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -9,6 +9,12 @@ import Posts from "./pages/Posts";
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/posts">Posts</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About/>}></Route>
