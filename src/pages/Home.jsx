77a1897c729655222de48c09c0a8f3587073c2ce
Emmanuel Bedzo <bedzoemmanuel8@gmail.com>
2025-05-24 21:15:27 -0500

    Working with APIs in React

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
-function Home(){
-    return <h1>Home</h1>
+import { useEffect, useState } from "react";
+import axios from "axios";
+function Home() {
+    const [users, setUsers] = useState([])
+  async function fetchData() {
+    const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
+    setUsers(data)
+    
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <h1>{users.length > 0 ? users[0].username: null}</h1>;
 }
 
-export default Home
+export default Home;
