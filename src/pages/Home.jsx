a8a48207f8709c574bdab6297898ce8edd2d86e6
Emmanuel Bedzo <bedzoemmanuel8@gmail.com>
2025-05-24 22:08:35 -0500

    Sending each date to their unique routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import User from "../components/User"
+import User from "../components/User";
+import { Link } from "react-router-dom";
 function Home() {
   const [users, setUsers] = useState([]);
   async function fetchData() {
@@ -16,7 +17,15 @@ function Home() {
   return (
     <div>
       {users.map((user) => (
-        <User key={user.id} id={user.id} name={user.name} email={user.email} username={user.username}/>
+        <Link key={user.id} to={`/users/${user.id}`}>
+        <User
+          
+          id={user.id}
+          name={user.name}
+          email={user.email}
+          username={user.username}
+        />
+        </Link>
       ))}
     </div>
   );
